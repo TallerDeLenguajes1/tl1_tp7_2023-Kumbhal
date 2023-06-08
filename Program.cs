044c5642fdb1e433b34afedcd5a86f3d4abec04b
isas321 <javier.isas321@gmail.com>
2023-06-08 18:04:25 -0300

    Sin errores en programa para cargar hasta fdn

--- Program.cs
+++ Program.cs
@@ -1,3 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EstadoEmpleado;
-Empleados nuevo = new Empleados();
+bool funciona = false;
+string? stringNum = ""; 
+int anio = 0;
+int mes = 0;
+int dia = 0;
+Empleados EmpleadoNuevo = new Empleados();
+while (!funciona){
+    Console.Write("Ingrese el nombre del empleado: ");
+    stringNum = Console.ReadLine();
+    if (stringNum == null){
+        Console.WriteLine("Nombre invalido");
+    }else{
+        funciona = true;
+    }
+}
+EmpleadoNuevo.nombre = stringNum;
+funciona = false;
+while (!funciona){
+    Console.Write("Ingrese el apellido del empleado: ");
+    stringNum = Console.ReadLine();
+    if (stringNum == null){
+        Console.WriteLine("Apellido invalido");
+    }else{
+        funciona = true;
+    }
+}
+EmpleadoNuevo.apellido = stringNum;
+funciona = false;
+while (!funciona){
+    Console.WriteLine("Ingrese el dia de nacimiento: ");
+    int.TryParse(stringNum, out anio);
+    if (anio <= 0){
+        Console.WriteLine("Ingrese un anio valido.");
+    }else{
+        Console.WriteLine("Ingres el mes de nacimiento: ");
+        int.TryParse(stringNum, out mes);
+        if (mes < 1 | mes > 12){
+            Console.WriteLine("Ingrese un mes valido.");
+        }else{
+            Console.WriteLine("Ingrese dia de nacimiento: ");
+            int.TryParse(stringNum, out dia);
+            if (dia < 1 | dia > 31){
+                Console.WriteLine("Ingrese un dia valido.");
+            }else{
+                funciona = true;
+            }
+        }
+    }
+}
+EmpleadoNuevo.fdn = new DateTime(anio, mes, dia);
